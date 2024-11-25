04a6e81cda8bb60427533d0bc41f73679a9d2be6
2CMINHOCA22 <00001095389142sp@al.educacao.sp.gov.br>
2024-11-24 21:32:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'O que é uma célula procarionte?',
+    'É uma célula que não possui núcleo definido, com o material genético disperso no citoplasma.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a diferença entre mitose e meiose?',
+    'Mitose: Divisão celular que gera duas células geneticamente idênticas, essencial para o crescimento e a reparação. Meiose: Divisão celular que reduz pela metade o número de cromossomos formando células gaméticas (sexuais).'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a fução principal dos ribossomos?',
+    'Os ribossomos são responsáveis pela síntese de proteínas, utilizando o RNA mensageiro como molde.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'É um processo pelo qual as plantas, algas e algumas bactérias produzem glicose e oxigênio a partir de dióxido de carbono, água e luz solar.'
+)
